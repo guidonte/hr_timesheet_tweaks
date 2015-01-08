6beffb377f6392d93331988e4c3ba387de9c1236
Guido Amoruso <guidonte@gmail.com>
2015-01-08 16:05:12 +0100

    Initial commit.

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Hr Timesheet Tweaks - Goodora',
+    'description': 'Tweaks for hr_timesheet by Goodora s.r.l.',
+    'author': 'Goodora s.r.l.',
+    'version': '0.1',
+    'category': 'Hidden',
+    'depends': [
+        'hr',
+        'hr_timesheet',
+        'hr_timesheet_invoice',
+    ],
+    'data': [
+        'hr_timesheet_tweaks_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
